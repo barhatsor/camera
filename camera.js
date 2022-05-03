ed6a5b0a83a45a72762ac49b1d2ae1374a6addf9
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-05-03 16:38:07 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -294,7 +294,7 @@ document.querySelector('#gallery-input').addEventListener('change', function() {
       
       const thumbnailURL = await blobToDataURL(file);
             
-      galleryWrapper.innerHTML += '<video src="' + URL.createObjectURL(file) + '" style="background-image:url(\'' + thumbnailURL + '\')" onclick="this.play()" onpause="this.focus()" class="picture" crossorigin="anonymous"></video>';
+      galleryWrapper.innerHTML += '<video src="' + URL.createObjectURL(file) + '" style="background-image:url(\'' + thumbnailURL + '\')" onclick="this.play()" onpause="this.click()" class="picture" crossorigin="anonymous"></video>';
       
     });
     
